4872ef6ea286b9b3a13d1cfff6629263e7eae5ec
Denionline <danielximenes164@gmail.com>
2025-05-20 18:13:38 +0100

    feat: start to sort

--- push_swap.c
+++ push_swap.c
@@ -6,19 +6,21 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:27:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/05/20 14:32:31 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/05/20 18:12:32 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/push_swap.h"
 
-
-
 void	push_swap(t_node **a, t_node **b)
 {
-	unsigned int	i;
+	t_list			*temp;
 
-	i = 0;
-	while (i++ < (*a)->length / 2)
-		ft_pn(a, b);
+	temp = (*a)->top;
+	while (temp != NULL)
+	{
+		if (temp->number > temp->next->number)
+			ft_pn(a, b);
+		temp = temp->next;
+	}
 }
