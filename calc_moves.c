b95865b86f03bf368e2a11153ef153b85948f254
Denionline <danielximenes164@gmail.com>
2025-05-29 18:30:43 +0100

    Remove all code, to write again :)

--- calc_moves.c
+++ calc_moves.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 12:36:21 by dximenes          #+#    #+#             */
-/*   Updated: 2025/05/29 09:26:59 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/05/29 16:46:12 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,4 @@
 
 int calc_moves(t_stack * a, t_stack * b, int number)
 {
-	int reverse_a;
-	int reverse_b;
-	int moves_a;
-	int moves_b;
-	int pos_a;
-	int pos_b;
-	int amount;
-
-	pos_a = get_pos(a, number, FALSE);
-	reverse_a = pos_a > (a->length / 2);
-	moves_a = pos_a;
-	if (reverse_a)
-		moves_a = a->length - pos_a;
-	pos_b = get_pos(b, number, TRUE);
-	reverse_b = pos_b > (b->length / 2);
-	moves_b = pos_b;
-	if (reverse_b)
-		moves_b = b->length - pos_b;
-	amount = 1;
-	if (reverse_a == reverse_b)
-	{
-		if (moves_a > moves_b)
-			amount += moves_a;
-		amount += moves_b;
-	}
-	else
-		amount += (moves_a + moves_b);
-	return (amount);
 }
--- include/push_swap.h
+++ include/push_swap.h
@@ -6,40 +6,40 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 15:38:54 by dximenes          #+#    #+#             */
-/*   Updated: 2025/05/28 15:08:40 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/05/29 16:46:17 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#define PUSH_SWAP_H
 
-# include "stdio.h"
-# include "libft/include/libft.h"
+#include "libft/include/libft.h"
+#include "stdio.h"
 
-# define TRUE 1
-# define FALSE 0
+#define TRUE 1
+#define FALSE 0
 
 typedef struct s_stack
 {
-	char	*name;
-	int		*array;
-	int		length;
-}	t_stack;
+	char * name;
+	int *  array;
+	int	   length;
+} t_stack;
 
-int		main(int argc, char *argv[]);
-void	push_swap(t_stack *a, t_stack *b, int print);
+int	 main(int argc, char * argv[]);
+void push_swap(t_stack * a, t_stack * b, int print);
 
-//Operations
-void	sn(t_stack **n, int toprint);
-void	ss(t_stack **a, t_stack **b, int toprint);
-void	pn(t_stack **from, t_stack **to, int toprint);
-void	rn(t_stack **n, int toprint);
-void	rr(t_stack **a, t_stack **b, int toprint);
-void	rrn(t_stack **n, int toprint);
-void	rrr(t_stack **a, t_stack **b, int toprint);
+// Operations
+void sn(t_stack ** n, int toprint);
+void ss(t_stack ** a, t_stack ** b, int toprint);
+void pn(t_stack ** from, t_stack ** to, int toprint);
+void rn(t_stack ** n, int toprint);
+void rr(t_stack ** a, t_stack ** b, int toprint);
+void rrn(t_stack ** n, int toprint);
+void rrr(t_stack ** a, t_stack ** b, int toprint);
 
-//helpers
-int		calc_moves(t_stack * a, t_stack * b, int number);
-int		get_pos(t_stack * n, int number, int searchbetween);
-void	fh_print(t_stack *a, t_stack *b, int amount);
+// helpers
+int	 calc_moves(t_stack * a, t_stack * b, int number);
+int	 get_pos(t_stack * n, int number, int searchbetween);
+void fh_print(t_stack * a, t_stack * b, int amount);
 #endif
