9fdda2a653cf61a2d47de419b7bd0353b87dbf86
Denionline <danielximenes164@gmail.com>
2025-05-30 10:00:10 +0100

    feat: up

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:27:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/05/30 09:40:37 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/05/30 09:57:22 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,27 @@
 
 void push_swap(t_stack * a, t_stack * b, int print)
 {
-	int	lesser;
-	int	lesser_moves;
-	int	i;
+	int current_moves;
+	int lesser_moves;
+	int lesser;
+	int i;
 
 	pn(&a, &b, print);
 	pn(&a, &b, print);
 	while (a->length > 3)
 	{
-		lesser = a->array;
+		i = 0;
+		lesser_moves = calc_moves(a, b, a->array[i]);
+		while (i < a->length)
+		{
+			current_moves = calc_moves(a, b, a->array[i]);
+			if (lesser_moves > current_moves)
+			{
+				lesser = a->array[i];
+				lesser_moves = current_moves;
+			}
+			i++;
+		}
+
 	}
 }
