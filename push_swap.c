5799ecc1f78edbd094b943409bf59fde4b946e63
Denionline <danielximenes164@gmail.com>
2025-06-05 19:01:53 +0100

    feat: Sorting working with the media of 5814

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:27:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/05 17:59:14 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/05 19:01:02 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,52 +87,83 @@ void push_swap(t_stack * a, t_stack * b)
 {
 	int	  chunk_size;
 	int * sorted;
-	int	  chunk_start, chunk_end;
+	int	  chunk_start;
+	int	  chunk_end;
+	int	  val;
+	int	  max;
+	int	  pos;
+	int	  i;
+	int	  found;
+	int	  j;
+	int	  total_len;
 
-	if (a->length <= 100)
-		chunk_size = 20;
+	total_len = a->length;
+	if (total_len <= 100)
+		chunk_size = 10;
 	else
-		chunk_size = 45;
+		chunk_size = 50;
 	sorted = get_sorted_array(a);
 
 	chunk_start = 0;
-	while (chunk_start < a->length)
+	while (chunk_start < total_len)
 	{
 		chunk_end = chunk_start + chunk_size;
-		if (chunk_end > a->length)
-			chunk_end = a->length;
+		if (chunk_end > total_len)
+			chunk_end = total_len;
 
-		int i = 0;
-		int count = 0;
-		int len = a->length;
-		while (i < len && count < (chunk_end - chunk_start))
+		found = 1;
+		while (found && a->length > 0)
 		{
-			int val = a->array[0];
+			found = 0;
+			j = 0;
+			while (j < a->length)
+			{
+				val = a->array[j];
+				if (val >= sorted[chunk_start] && val < sorted[chunk_end])
+				{
+					found = 1;
+					break;
+				}
+				j++;
+			}
+			if (!found)
+				break;
+			val = a->array[0];
 			if (val >= sorted[chunk_start] && val < sorted[chunk_end])
 			{
-				pn(a, b); // pb
-				count++;
+				pn(a, b);
+				// Se o valor for menor que a média do chunk, rotacione b
+				if (val < sorted[chunk_start] + (chunk_end - chunk_start) / 2)
+					rn(b, TRUE);
 			}
 			else
-				rn(a, TRUE); // ra
-			i++;
+				rn(a, TRUE);
 		}
 		chunk_start = chunk_end;
 	}
 	free(sorted);
 
-	// Agora traga de volta para a, sempre o maior de b
+	// Se ainda restar algo em a, empurre tudo para b
+	while (a->length > 0)
+		pn(a, b);
+
 	while (b->length > 0)
 	{
-		int max = get_bigger(b);
-		int pos = get_pos(b, max);
+		max = get_bigger(b);
+		pos = get_pos(b, max);
 		if (pos <= b->length / 2)
-			while (get_pos(b, max) != 0)
-				rn(b, TRUE); // rb
+		{
+			i = 0;
+			while (get_pos(b, max) != 0 && i++ < b->length)
+				rn(b, TRUE);
+		}
 		else
-			while (get_pos(b, max) != 0)
-				rrn(b, TRUE); // rrb
-		pn(b, a);			  // pa
+		{
+			i = 0;
+			while (get_pos(b, max) != 0 && i++ < b->length)
+				rrn(b, TRUE);
+		}
+		pn(b, a);
 	}
 	put_on_top(a, get_smaller(a));
 }
