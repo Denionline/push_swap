8ca8e2670eab3612496be0a31916ab1e7c93b567
Denionline <danielximenes164@gmail.com>
2025-06-08 13:22:35 +0100

    feat: Updating the test.sh

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 15:37:48 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/07 18:28:27 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/08 11:13:59 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,10 @@ int fh_issrted(t_stack * a, t_stack * b)
 		for (int j = i; j < a->length; j++)
 		{
 			if (a->array[i] > a->array[j])
-				return (0);
+				return (FALSE);
 		}
 	}
-	return (1);
+	return (TRUE);
 }
 
 int main(int argc, char * argv[])
@@ -61,9 +61,13 @@ int main(int argc, char * argv[])
 	char **	  values;
 	int		  size;
 	int		  i;
+	int		  just_print_sort;
 
 	if (argc <= 1)
 		return (0);
+	just_print_sort = 0;
+	if (*argv[1] == 's')
+		just_print_sort = (argv++ != NULL);
 	values = fh_getarray(argc, argv);
 	size = 0;
 	while (values[size])
@@ -78,5 +82,9 @@ int main(int argc, char * argv[])
 	}
 	free(values);
 	push_swap(a, b);
+	if (just_print_sort && fh_issrted(a, b))
+		ft_putendl_fd("\033[0;92mIs sorted :D\033[0;39m", 1);
+	else if (just_print_sort)
+		ft_putendl_fd("\033[0;91mIs not sorted :(\033[0;39m", 1);
 	return (0);
 }
