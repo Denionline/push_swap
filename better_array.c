50e94985724773f19780a9ec963b9b704fdc1fee
Denionline <danielximenes164@gmail.com>
2025-06-11 11:24:12 +0100

    feat: Updating

--- better_array.c
+++ better_array.c
@@ -1,98 +1,95 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   better_array.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/09 18:50:40 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/09 20:19:05 by dximenes         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   better_array.c                                     :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/06/09 18:50:40 by dximenes          #+#    #+#             */
+// /*   Updated: 2025/06/11 11:18:52 by dximenes         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "include/push_swap.h"
+// #include "include/push_swap.h"
 
-static int get_next(int * array, int len, int number)
-{
-	int i;
-	int actual;
+// static int get_next(int * array, int len, int number)
+// {
+// 	int i;
+// 	int actual;
 
-	actual = number;
-	i = 0;
-	while (i < len)
-	{
-		if (array[i] > actual)
-			actual = array[i];
-		i++;
-	}
-	i = 0;
-	while (i < len)
-	{
-		if (array[i] < actual && array[i] > number)
-			actual = array[i];
-		i++;
-	}
-	return (actual);
-}
+// 	actual = number;
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		if (array[i] > actual)
+// 			actual = array[i];
+// 		i++;
+// 	}
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		if (array[i] < actual && array[i] > number)
+// 			actual = array[i];
+// 		i++;
+// 	}
+// 	return (actual);
+// }
 
-static int * get_better(int * array, int * better, int len)
-{
-	int j;
-	int i;
+// static void get_better(int * array, t_moves * moves)
+// {
+// 	int j;
+// 	int i;
 
-	j = 0;
-	i = 0;
-	better[j] = array[i];
-	while (i < len)
-	{
-		if (array[i] != get_next(array + i, len, array[i]))
-			better[++j] = get_next(array + i, len, array[i]);
-		i++;
-	}
-	return (better);
-}
+// 	j = 0;
+// 	i = 0;
+// 	moves->better[j] = array[i];
+// 	while (i < moves->better_len)
+// 	{
+// 		if (array[i] < get_next(array + i, moves->better_len, array[i]))
+// 			moves->better[++j] = get_next(array + i, moves->better_len, array[i]);
+// 		i++;
+// 	}
+// }
 
-static int get_possibilities(int * array, int len)
-{
-	int amount;
-	int i;
+// static int get_possibilities(int * array, int len)
+// {
+// 	int amount;
+// 	int i;
 
-	amount = 0;
-	i = 0;
-	while (i < len)
-	{
-		if (array[i] != get_next(array + i, len, array[i]))
-			amount++;
-		i++;
-	}
-	return (amount);
-}
+// 	amount = 0;
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		if (array[i] != get_next(array + i, len, array[i]))
+// 			amount++;
+// 		i++;
+// 	}
+// 	return (amount);
+// }
 
-int * better_array(t_stack * a, t_moves * moves)
-{
-	int * new_better;
-	int	  best_possibilities;
-	int	  possibilities;
-	int	  position;
-	int	  i;
+// void better_array(t_stack * a, t_moves * moves)
+// {
+// 	int best_possibilities;
+// 	int possibilities;
+// 	int position;
+// 	int i;
 
-	new_better = malloc((a->length / 2) * sizeof(int));
-	if (!new_better)
-		return (NULL);
-	best_possibilities = 0;
-	position = 0;
-	i = 0;
-	while (i < a->length)
-	{
-		possibilities = get_possibilities(a->array + i, a->length);
-		if (possibilities > best_possibilities)
-		{
-			best_possibilities = possibilities;
-			position = i;
-		}
-		i++;
-	}
-	moves->better_len = best_possibilities;
-	new_better = get_better(a->array + position, new_better, moves->better_len);
-	return (new_better);
-}
+// 	moves->better = malloc((a->length / 2) * sizeof(int));
+// 	if (!moves->better)
+// 		return;
+// 	best_possibilities = 0;
+// 	position = 0;
+// 	i = 0;
+// 	while (i < a->length)
+// 	{
+// 		possibilities = get_possibilities(a->array + i, a->length);
+// 		if (possibilities > best_possibilities)
+// 		{
+// 			best_possibilities = possibilities;
+// 			position = i;
+// 		}
+// 		i++;
+// 	}
+// 	moves->better_len = best_possibilities;
+// 	get_better(a->array + position, moves);
+// }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:27:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/09 19:32:19 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/11 11:22:27 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,19 +66,19 @@ static void put_on_top(t_stack * n, int number)
 	}
 }
 
-static int is_on_better(int number, t_moves * moves)
-{
-	int i;
+// static int is_on_better(int number, t_moves * moves)
+// {
+// 	int i;
 
-	i = 0;
-	while (i < moves->better_len)
-	{
-		if (moves->better[i] == number)
-			return (1);
-		i++;
-	}
-	return (0);
-}
+// 	i = 0;
+// 	while (i < moves->better_len)
+// 	{
+// 		if (moves->better[i] == number)
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
+// }
 
 void push_swap(t_stack * a, t_stack * b)
 {
@@ -88,26 +88,21 @@ void push_swap(t_stack * a, t_stack * b)
 
 	pn(a, b);
 	pn(a, b);
-	moves.better = better_array(a, &moves);
+	// better_array(a, &moves);
 	while (a->length > 3)
 	{
 		i = 0;
 		moves = calc_moves(a, b, a->array[i]);
-		while (!is_on_better(a->array[i++], &moves) && i < a->length)
+		while (++i < a->length)
 		{
 			current_moves = calc_moves(a, b, a->array[i]);
 			if (moves.total > current_moves.total)
 				moves = current_moves;
 		}
-		if (is_on_better(a->array[i++], &moves))
-			rn(a, TRUE);
-		else
-		{
-			if (moves.reverse_a == moves.reverse_b)
-				rotate_two(a, b, &moves);
-			rotate_one(a, b, &moves);
-			pn(a, b);
-		}
+		if (moves.reverse_a == moves.reverse_b)
+			rotate_two(a, b, &moves);
+		rotate_one(a, b, &moves);
+		pn(a, b);
 	}
 	order_last_three(a);
 	get_back(a, b);
