7692403518204ff5671e239de37d93f72f8f05a6
Denionline <danielximenes164@gmail.com>
2025-06-13 12:04:22 +0100

    feat: WORKING WITH LESS THAN THE NECESSARYYY :tada:

--- push_swap.c
+++ push_swap.c
@@ -6,47 +6,47 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:27:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/13 09:53:38 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/13 11:44:47 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/push_swap.h"
 
-static void order_last_three(t_stack * a)
-{
-	if (a->array[0] > a->array[1] && a->array[1] > a->array[2])
-		rn(a, TRUE);
-	else if (a->array[2] < a->array[1] && a->array[2] > a->array[0])
-		rrn(a, TRUE);
-	else if (a->array[0] > a->array[1] && a->array[0] > a->array[2])
-		rn(a, TRUE);
-	else if (a->array[2] < a->array[0] && a->array[2] < a->array[1])
-		rrn(a, TRUE);
-	if (a->array[0] > a->array[1])
-		sn(a, TRUE);
-}
+// static void order_last_three(t_stack * a)
+// {
+// 	if (a->array[0] > a->array[1] && a->array[1] > a->array[2])
+// 		rn(a, TRUE);
+// 	else if (a->array[2] < a->array[1] && a->array[2] > a->array[0])
+// 		rrn(a, TRUE);
+// 	else if (a->array[0] > a->array[1] && a->array[0] > a->array[2])
+// 		rn(a, TRUE);
+// 	else if (a->array[2] < a->array[0] && a->array[2] < a->array[1])
+// 		rrn(a, TRUE);
+// 	if (a->array[0] > a->array[1])
+// 		sn(a, TRUE);
+// }
 
-static void get_back(t_stack * a, t_stack * b)
-{
-	int moves_b;
-	int i;
+// static void get_back(t_stack * a, t_stack * b)
+// {
+// 	int moves_b;
+// 	int i;
 
-	moves_b = 0;
-	while (b->length > 0)
-	{
-		i = 0;
-		moves_b = get_bigger_pos(a, b->array[0]);
-		if (moves_b > (a->length / 2))
-			while (i++ < (a->length - moves_b))
-				rrn(a, TRUE);
-		else
-			while (i++ < moves_b)
-				rn(a, TRUE);
-		pn(b, a);
-		if (a->array[0] > a->array[1])
-			sn(a, TRUE);
-	}
-}
+// 	moves_b = 0;
+// 	while (b->length > 0)
+// 	{
+// 		i = 0;
+// 		moves_b = get_bigger_pos(a, b->array[0]);
+// 		if (moves_b > (a->length / 2))
+// 			while (i++ < (a->length - moves_b))
+// 				rrn(a, TRUE);
+// 		else
+// 			while (i++ < moves_b)
+// 				rn(a, TRUE);
+// 		pn(b, a);
+// 		if (a->array[0] > a->array[1])
+// 			sn(a, TRUE);
+// 	}
+// }
 
 static void put_on_top(t_stack * n, int number)
 {
@@ -66,58 +66,58 @@ static void put_on_top(t_stack * n, int number)
 	}
 }
 
-// static int is_on_better(int number, t_moves * moves)
-// {
-// 	int i;
+static int is_on_better(int number, t_moves * moves)
+{
+	int i;
 
-// 	i = 0;
-// 	while (i < moves->better_len)
-// 	{
-// 		if (moves->better[i] == number)
-// 			return (TRUE);
-// 		i++;
-// 	}
-// 	return (FALSE);
-// }
+	i = 0;
+	while (i < moves->better_len)
+	{
+		if (moves->better[i] == number)
+			return (TRUE);
+		i++;
+	}
+	return (FALSE);
+}
 
-// int * ft_find_lis(int * array, int len, int * lis_len)
-// {
-// 	int * dp = malloc(len * sizeof(int));
-// 	int * prev = malloc(len * sizeof(int));
-// 	int	  max = 0, max_idx = 0;
-// 	int * lis;
-// 	int	  i, j, k;
+int * ft_find_lis(int * array, int len, int * lis_len)
+{
+	int * dp = malloc(len * sizeof(int));
+	int * prev = malloc(len * sizeof(int));
+	int	  max = 0, max_idx = 0;
+	int * lis;
+	int	  i, j, k;
 
-// 	for (i = 0; i < len; i++)
-// 	{
-// 		dp[i] = 1;
-// 		prev[i] = -1;
-// 		for (j = 0; j < i; j++)
-// 		{
-// 			if (array[j] < array[i] && dp[j] + 1 > dp[i])
-// 			{
-// 				dp[i] = dp[j] + 1;
-// 				prev[i] = j;
-// 			}
-// 		}
-// 		if (dp[i] > max)
-// 		{
-// 			max = dp[i];
-// 			max_idx = i;
-// 		}
-// 	}
-// 	*lis_len = max;
-// 	lis = malloc(max * sizeof(int));
-// 	k = max - 1;
-// 	while (max_idx >= 0)
-// 	{
-// 		lis[k--] = array[max_idx];
-// 		max_idx = prev[max_idx];
-// 	}
-// 	free(dp);
-// 	free(prev);
-// 	return lis;
-// }
+	for (i = 0; i < len; i++)
+	{
+		dp[i] = 1;
+		prev[i] = -1;
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] < array[i] && dp[j] + 1 > dp[i])
+			{
+				dp[i] = dp[j] + 1;
+				prev[i] = j;
+			}
+		}
+		if (dp[i] > max)
+		{
+			max = dp[i];
+			max_idx = i;
+		}
+	}
+	*lis_len = max;
+	lis = malloc(max * sizeof(int));
+	k = max - 1;
+	while (max_idx >= 0)
+	{
+		lis[k--] = array[max_idx];
+		max_idx = prev[max_idx];
+	}
+	free(dp);
+	free(prev);
+	return lis;
+}
 
 void push_swap(t_stack * a, t_stack * b)
 {
@@ -125,30 +125,30 @@ void push_swap(t_stack * a, t_stack * b)
 	t_moves moves;
 	int		i;
 
-	// moves.better = ft_find_lis(a->array, a->length, &moves.better_len);
-	// while (a->length > moves.better_len)
-	// {
-	// 	if (is_on_better(a->array[0], &moves))
-	// 		rn(a, TRUE);
-	// 	else
-	// 		pn(a, b);
-	// }
-	while (a->length > 0)
+	moves.better = ft_find_lis(a->array, a->length, &moves.better_len);
+	while (a->length > moves.better_len)
 	{
-		moves = calc_moves(a, b, a->array[0]);
+		if (is_on_better(a->array[0], &moves))
+			rn(a, TRUE);
+		else
+			pn(a, b);
+	}
+	while (b->length > 0)
+	{
+		moves = calc_moves(b, a, b->array[0]);
 		i = 0;
-		while (++i < a->length)
+		while (++i < b->length)
 		{
-			current_moves = calc_moves(a, b, b->array[i]);
+			current_moves = calc_moves(b, a, b->array[i]);
 			if (moves.total > current_moves.total)
 				moves = current_moves;
 		}
 		if (moves.reverse_a == moves.reverse_b)
-			rotate_two(a, b, &moves);
-		rotate_one(a, b, &moves);
-		pn(a, b);
+			rotate_two(b, a, &moves);
+		rotate_one(b, a, &moves);
+		pn(b, a);
 	}
-	order_last_three(a);
-	get_back(a, b);
+	// order_last_three(a);
+	// get_back(a, b);
 	put_on_top(a, get_smaller(a));
 }
